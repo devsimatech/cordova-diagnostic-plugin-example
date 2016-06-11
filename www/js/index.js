f1525c395de12a7e9b0b12c5713c7dfa355deed2
Dave Alden <dave@workingedge.co.uk>
2016-06-11 09:21:57 +0100

    Remove erroneous reference

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,6 @@ function onDeviceReady() {
     $('#listen').on("click", function(){
         cordova.plugins.diagnostic.registerLocationStateChangeHandler(function(state){
             console.log("Location state changed to: "+state);
-            checkState();
         }, function(error){
             console.error("Error registering for location state changes: "+error);
         });
